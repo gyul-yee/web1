c809285cf9f5e2ac8631e468d25a7ec6a8ffae40
kys0411 <32180846@dankook.ac.kr>
2022-08-06 18:13:35 +0900

    gradle 적용

---
+++ src/main/java/dku/meronamap/MeronamapApplication.java
@@ -0,0 +1,13 @@
+package dku.meronamap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeronamapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeronamapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dku/meronamap/MeronamapApplicationTests.java
@@ -0,0 +1,13 @@
+package dku.meronamap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeronamapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
